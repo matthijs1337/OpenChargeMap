f361150b2d61c9bd68fe461897746e5291ee5a25
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 20:19:06 +0200

    Update Case3Groep5.py

--- Case3Groep5.py
+++ Case3Groep5.py
@@ -49,13 +49,24 @@ st.write(f'De gekozen plek {result}')
 
 for obj in combo_list:
    if (result == obj.provincie):
-      querystring = {"latitude": obj.latitude,"longitude":obj.longitude, "output": "json", "compact": True, "verbose": False}
+      querystring = {"latitude": obj.latitude,"longitude":obj.longitude,  "countrycode": "NL", "output": "json", "compact": True, "verbose": False}
 
 st.write(f'{querystring}')
-        
+
+########################
+#url = "https://api.openchargemap.io/v3/poi"
+
+#params = {"latitude": 52.0907374, "longitude": -5.1214201, "countrycode": "NL", "output": "json", "compact": True, "verbose": False}
+
+#headers = {"Content-Type": "application/json", "X-API-Key": "2401ef11-fde1-4b32-a14f-16f0244ddd38"}
+
+#response = requests.request("GET", url, headers=headers, params=params)
+
+
+#############################
+
 response = requests.request("GET", url, headers=headers, params=querystring)
-print(response.text)
-tekst = response.json()
-Open_Charge_Map=pd.DataFrame(tekst)
 st.title("Hoi")
-st.dataframe(Open_Charge_Map)
+json=response.json()
+Open_Charge_Map=pd.DataFrame(json)
+Open_Charge_Map.head()
