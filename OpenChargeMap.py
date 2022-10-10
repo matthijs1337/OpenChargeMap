2aaee9129736e5fa1fa015c9bd0f0f722ca8306f
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 11:33:19 +0200

    Update OpenChargeMap.py

--- OpenChargeMap.py
+++ OpenChargeMap.py
@@ -20,8 +20,14 @@ import plotly.io as pio
 pio.renderers.default = 'browser'
 
 #url van api
-url = https://api.openchargemap.io/v3
 
+response = requests.get('https://api.openchargemap.io/v3')
+
+response
+headers = {
+ #"X-RapidAPI-Key":"f63be25ec8msh09be29d15fcc4fbp1f0f6ejsn82d64b7f3674",
+ "X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
+ "X-RapidAPI-Host": "https://openchargemap.org/site"}
 
 
 
