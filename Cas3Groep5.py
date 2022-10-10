dd4600a52776372b228d4ca1ecb06f139df2dd50
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 12:58:54 +0200

    Update Cas3Groep5.py

--- Cas3Groep5.py
+++ Cas3Groep5.py
@@ -4,30 +4,20 @@ Created on Mon Oct 10 10:59:05 2022
 
 @author: matth
 """
-
-#importeren van functies
-
 import pandas as pd
 import requests
-import streamlit as st
-import plotly.graph_objects as go
-import plotly.express as px
+import json
 
-import datetime as dt
-from dateutil.relativedelta import relativedelta # to add days or years
+url = "https://api.openchargemap.io/v3/poi"
 
-import plotly.io as pio
-pio.renderers.default = 'browser'
+params = {"latitude": 52.0907374, "longitude": -5.1214201, "countrycode": "NL", 
 
-#url van api
+"output": "json", "compact": True, "verbose": False}
 
-response = requests.get('https://api.openchargemap.io/v3')
+headers = {"Content-Type": "application/json", "X-API-Key": "2401ef11-fde1-4b32-a14f-16f0244ddd38"}
 
-response
-headers = {
- #"X-RapidAPI-Key":"f63be25ec8msh09be29d15fcc4fbp1f0f6ejsn82d64b7f3674",
- "X-RapidAPI-Key": "a929aa606bmsh42432cc9b369422p1b8238jsnce8e2abf2f38",
- "X-RapidAPI-Host": "https://openchargemap.org/site"}
+response = requests.request("GET", url, headers=headers, params=params)
 
-
-
+json=response.json()
+Open_Charge_Map=pd.DataFrame(json)
+Open_Charge_Map.head()
