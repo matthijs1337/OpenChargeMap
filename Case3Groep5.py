32b5d1b074c2df53bdb0e5dae1389974b0fdae78
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 13:14:37 +0200

    Update Case3Groep5.py

--- Case3Groep5.py
+++ Case3Groep5.py
@@ -12,14 +12,40 @@ import json
 
 url = "https://api.openchargemap.io/v3/poi"
 
-params = {"latitude": 52.0907374, "longitude": -5.1214201, "countrycode": "NL", 
 
 "output": "json", "compact": True, "verbose": False}
 
 headers = {"Content-Type": "application/json", "X-API-Key": "2401ef11-fde1-4b32-a14f-16f0244ddd38"}
 
-response = requests.request("GET", url, headers=headers, params=params)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
 json=response.json()
 Open_Charge_Map=pd.DataFrame(json)
 Open_Charge_Map.head()
+
+#klasse gemeente
+class gemeente:
+  def __init__ (self, gemeente, latitude, longitude):
+    self.gemeente = gemeente
+    self.longitude = longitude
+    self.latitude = latitude
+    def show_all(self):
+        print (self.land, self.latitude, self.longitude)
+        
+        
+combo_list=[]
+original_list = []
+combo_list.append(gemeente("Amsterdam", 52.3667, 4.8945))
+
+
+
+
+for obj in combo_list:
+  original_list.append(obj.gemeente)
+
+#radio
+result = st.sidebar.selectbox('Select a city', original_list)
+
+for obj in combo_list:
+    if (result == obj.gemeente):
+      querystring = {"lat": obj.latitude,"lon":obj.longitude}
