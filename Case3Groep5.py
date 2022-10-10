ca37b755808188e56d473a715b0f7719107e37ea
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 13:20:01 +0200

    Update Case3Groep5.py

--- Case3Groep5.py
+++ Case3Groep5.py
@@ -5,14 +5,19 @@ Created on Mon Oct 10 10:59:05 2022
 @author: matth
 """
 #Streamlit link: https://matthijs1337-openchargemap-case3groep5-j60f5d.streamlitapp.com/
-
 import pandas as pd
 import requests
 import json
 
 url = "https://api.openchargemap.io/v3/poi"
 
+params = {"latitude": 52.0907374, "longitude": -5.1214201, "countrycode": "NL", 
+
+"output": "json", "compact": True, "verbose": False}
+
 headers = {"Content-Type": "application/json", "X-API-Key": "2401ef11-fde1-4b32-a14f-16f0244ddd38"}
+
+response = requests.request("GET", url, headers=headers, params=params)
 
 json=response.json()
 Open_Charge_Map=pd.DataFrame(json)
@@ -44,5 +49,3 @@ result = st.sidebar.selectbox('Select a city', original_list)
 for obj in combo_list:
     if (result == obj.gemeente):
       querystring = {"lat": obj.latitude,"lon":obj.longitude}
-      
-response = requests.request("GET", url, headers=headers, params=querystring)
