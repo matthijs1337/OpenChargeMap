5dbb7e1850c1a79a29aa4f5adcc8bc38f6ef0647
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-10 14:31:43 +0200

    Update Case3Groep5.py

--- Case3Groep5.py
+++ Case3Groep5.py
@@ -35,11 +35,11 @@ url = "https://api.openchargemap.io/v3/poi"
 
 #params = {"latitude": 52.0907374, "longitude": -5.1214201, "countrycode": "NL", 
 
-#"output": "json", "compact": True, "verbose": False}
+params2= {"output": "json", "compact": True, "verbose": False}
 
 headers = {"Content-Type": "application/json", "X-API-Key": "2401ef11-fde1-4b32-a14f-16f0244ddd38"}
 
-response = requests.request("GET", url, headers=headers, params=(querystring, "output":"json", "compact": True, "verbose": False)
+response = requests.request("GET", url, headers=headers, params=(querystring, params2)
 
 json=response.json()
 Open_Charge_Map=pd.DataFrame(json)
