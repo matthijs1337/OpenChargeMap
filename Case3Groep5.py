3b30b1e9fc7d9f7bd29807f86375fb01b720b96f
matthijs1337 <114393291+matthijs1337@users.noreply.github.com>
2022-10-12 18:01:24 +0200

    Update Case3Groep5.py

--- Case3Groep5.py
+++ Case3Groep5.py
@@ -15,6 +15,15 @@ from folium.plugins import MarkerCluster
 import streamlit as st
 from streamlit_folium import st_folium
 
+#klas
+class Provincie:
+  def __init__ (self, provincie, longitude, latitude):
+    self.provincie = provincie
+    self.longitude = longitude
+    self.latitude = latitude
+    
+  def show_all(self):
+    print (self.provincie, self.longitude, self.latitude)
 
 # In[2]:
 
@@ -71,9 +80,34 @@ api_clean.rename(columns={'ID_y': 'ID', 'ID_x': 'Adress_ID', 'AddressLine1' : 'A
 
 
 # In[14]:
+#dropdownlijst
+    combo_list=[]
+    original_list = []
+    combo_list.append(Provincie("Noord-Holland", 52.375029, 4.630962))
+    combo_list.append(Provincie("Zuid-Holland", 52.090737, 5.121420))
+    combo_list.append(Provincie("Zeeland", 51.494030, 3.849681))
+    combo_list.append(Provincie("Noord-Brabant", 51.4826537, 5.232168))
+    combo_list.append(Provincie("Flevoland", 52.527978, 5.595350))
+    combo_list.append(Provincie("Overijssel", 52.438781, 6.501641))
+    combo_list.append(Provincie("Limburg", 51.442723, 6.060872))
+    combo_list.append(Provincie("Drenthe", 52.947601, 6.623058))
+    combo_list.append(Provincie("Groningen", 53.220028, 6.570447))
+    combo_list.append(Provincie("Friesland", 53.164164, 5.781754))
+    combo_list.append(Provincie("Gelderland", 52.04515, 5.871823))
+    combo_list.append(Provincie("Utrecht",52.1195, 5.1944))
+    
+    for obj in combo_list:
+      original_list.append(obj.provincie)
+    
+    result = st.selectbox('Selecteer de provincie', original_list)
+    st.write(f'De gekozen provincie {result}')
+    
+    for obj in combo_list:
+       if (result == obj.provincie):
+          querystring = {"latitude": obj.latitude,"longitude":obj.longitude}
 
 
-m = folium.Map(location=[52.377956, 4.897070], zoom_start=7)
+m = folium.Map(location=[querystring], zoom_start=7)
 
 marker_cluster = MarkerCluster().add_to(m)
 
